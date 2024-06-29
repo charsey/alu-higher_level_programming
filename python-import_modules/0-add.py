715ea181124c3fbde89c6c72ad34e2f6e9ffddef
charsey <charlesiheke2020@gmail.com>
2024-06-29 05:32:41 -0700

    made a change

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    
-def add(a, b):
-    return a + b
+
+a = 1
+b = 2
+
+from add_0 import add
+
+result = add(a, b)
+print("{} + {} = {}".format(a, b, result))
 
 
