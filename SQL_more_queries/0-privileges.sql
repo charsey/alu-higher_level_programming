7d03dde09b32c441d815a8c785a94b9777eaa13f
charsey <charlesiheke2020@gmail.com>
2024-07-21 02:13:37 -0700

    corrected the privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -3,3 +3,8 @@ SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
 -- List privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
+
+-- 0-privileges.sql
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
 -- 1-create_user.sql
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- 0-privileges.sql
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+
