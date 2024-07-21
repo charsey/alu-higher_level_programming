8fa1d8fb87739b5ab9d4dbc430919a99e2640ea4
charsey <charlesiheke2020@gmail.com>
2024-07-20 17:53:18 -0700

    create user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,10 @@
--- creates a user and password
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@localhost;
+-- Create users if they do not exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'password';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'password';
+
+-- Grant all privileges to users
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+
+-- Flush privileges to ensure changes take effect
 FLUSH PRIVILEGES;
