bbd2c9aefe442ccd2b4648f6b0142a5e7a1a69ca
charsey <charlesiheke2020@gmail.com>
2024-07-31 15:16:37 -0700

    edited 0-body_size

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Send request and get the size of the body of the response
-curl -s -o /dev/null -w '%{size_download}\n' "$1"
+# Script that shows the Content-Length from a HTTP request
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
